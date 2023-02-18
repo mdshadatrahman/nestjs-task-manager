f0a4bcbf176d55f464b419c958ec55fc14374dde
mdshadatrahman <shadat.rahman.464@gmail.com>
2023-02-18 23:39:43 +0600

    createTask

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,5 @@
 import { TaskService } from './task.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 
 @Controller('tasks')
@@ -9,5 +9,14 @@ export class TaskController {
 	@Get()
 	getAllTasks(): Task[] {
 		return this.taskService.getAllTasks();
+	}
+
+	@Post()
+	createTask(
+		@Body('title') title: string,
+		@Body('description') description: string
+
+	): Task {
+		return this.taskService.createTask(title, description);
 	}
 }
--- src/task/task.model.ts
+++ src/task/task.model.ts
@@ -6,7 +6,7 @@ export interface Task {
 }
 
 export enum TaskStatus {
-	OPNE = 'OPEN',
+	OPEN = 'OPEN',
 	IN_PROGRESS = 'IN_PROGRESS',
 	DONE = 'DONE',
 }
